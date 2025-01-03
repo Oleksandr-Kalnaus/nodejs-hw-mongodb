76bd6575993b9b4ece93d519ef7ae7d7d83fa1ba
Oleksandr-Kalnaus <oleksandrkalnaus@gmail.com>
2025-01-03 13:45:44 +0000

    Update validation schema

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -2,19 +2,6 @@ import Joi from 'joi';
 import { emailRegepx } from '../constants/users.js';
 
 export const userRegistrationSchema = Joi.object({
-
-  name: Joi.string().required().messages({
-    'string.required': 'Name is required.',
-    'string.empty': 'Name is required.',
-  }),
-  email: Joi.string().pattern(emailRegepx).required().messages({
-    'string.required': 'Email is required.',
-    'string.empty': 'Email is required.',
-  }),
-  password: Joi.string().required().messages({
-    'string.required': 'Password is required.',
-    'string.empty': 'Password is required.',
-=======
   name: Joi.string().required().min(3).max(20).messages({
     'string.required': 'Name is required.',
     'string.empty': 'Name is required.',
@@ -34,12 +21,11 @@ export const userRegistrationSchema = Joi.object({
 export const userLoginSchema = Joi.object({
   email: Joi.string().pattern(emailRegepx).required().messages({
     'string.required': 'Email is required.',
-
-    'string.empty': 'Email is required.',
+    'string.empty': 'Email should be looks: "example@mail.com".',
   }),
   password: Joi.string().required().messages({
     'string.required': 'Password is required.',
-    'string.empty': 'Password is required.',
+    'string.empty': 'Password should have at least 6 characters.',
   }),
 });
 
@@ -61,6 +47,5 @@ export const resetPasswordSchema = Joi.object({
   password: Joi.string().required().messages({
     'string.required': 'Password is required.',
     'string.empty': 'Password should have at least 6 characters.',
-
   }),
 });
